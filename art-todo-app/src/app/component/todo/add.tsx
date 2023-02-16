b75bf19dc9729b84290dca87e93ff673d948f5be
Abdulrehman <Abdurrehmantalat@outlook.com>
2023-02-16 23:56:57 +0500

    fixed add button issues

--- art-todo-app/src/app/component/todo/add.tsx
+++ art-todo-app/src/app/component/todo/add.tsx
@@ -10,24 +10,26 @@ export default function AddTodo({ }) {
         todoName: string
     }
     const { userId, setUserId, data, setData } = useGlobalContext();
-    const [ val, setVal ] = useState("");
-    const [ todoName, setTodoName ] = useState('');
-    function addName(){
-        let mytodo = {todoName:val};
-        setData([...data,mytodo]);
-        setTodoName('');
+    const [val, setVal] = useState("");
+    const [todoName, setTodoName] = useState('');
+    function addName() {
+        if (todoName != '') {
+            let mytodo = { todoName: val };
+            setData([...data, mytodo]);
+            setTodoName('');
+        }
     }
-    function inputChange(val: string){
-     setVal(val);
-     setTodoName(val);
+    function inputChange(val: string) {
+        setVal(val);
+        setTodoName(val);
     }
     return (
         <Box mt='-20'>
             <Box bg='#0987A0' mx={'auto'} w='80%' p={[8, 12, 14, 14, 20, 20]} color='white'>
                 <Stack spacing={[1, 2, 2, 2, 3, 4]} direction='row' align='center'>
-                    <Input variant='outline' value={todoName} onChange={(e)=> inputChange(e.target.value)} w={['320px', '375px', '425px', '768px', '1024px', '1740px']} height={['40']} size={['xs', 'sm', 'md', 'lg', 'lg', 'lg']} _placeholder={{ color: 'white' }} placeholder='add text here...' colorScheme='telegram' h={5} />
+                    <Input variant='outline' value={todoName} onChange={(e) => inputChange(e.target.value)} w={['320px', '375px', '425px', '768px', '1024px', '1740px']} height={['40']} size={['xs', 'sm', 'md', 'lg', 'lg', 'lg']} _placeholder={{ color: 'white' }} placeholder='add text here...' colorScheme='telegram' h={5} />
                     <WrapItem>
-                        <Button onClick={()=> addName()}  w={['38px', '32px', '70px', '86px', '70px', '220px']} h={['10']} size={['xs', 'sm', 'md', 'lg', 'lg', 'xl']} _hover={{ color: 'telegram' }} bg='#553C9A'>Add</Button>
+                        <Button onClick={() => addName()} w={['38px', '32px', '70px', '86px', '70px', '220px']} h={['10']} size={['xs', 'sm', 'md', 'lg', 'lg', 'xl']} _hover={{ color: 'telegram' }} bg='#553C9A'>Add</Button>
                     </WrapItem>
                 </Stack>
             </Box>
